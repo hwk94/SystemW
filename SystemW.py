e1466280fc284c9988d69cb6f3c3aab0d27c5044
hwk94 <haw94400@gmail.com>
2018-05-04 15:58:11 +0200

    Update SystemW.py

--- SystemW.py
+++ SystemW.py
@@ -1,1 +1,14 @@
-print ("hello");
+import socket
+
+s = socket.socket (
+    socket.AF_PACKET,
+    socket.SOCK_RAW,
+    socket.htons(3)
+  
+)
+
+s.bind(('ens0',3))
+
+while True:
+    message = s.recv(1024)
+    print(repr(message)
