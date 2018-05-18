2ab9c063b928cb75bf5b128893cb5d9967647788
hwk94 <haw94400@gmail.com>
2018-05-18 05:25:52 +0200

    Update SystemW.py
    Corrections des syntaxe et premiers test : capture des trames

--- SystemW.py
+++ SystemW.py
@@ -1,15 +1,15 @@
 import socket
 
-s = socket.socket (
-    socket.AF_PACKET,
-    socket.SOCK_RAW,
-    socket.htons(3)
-  
+s = socket.socket(
+        socket.AF_PACKET,
+        socket.SOCK_RAW,
+        socket.htons(3)
+
 )
 
-s.bind(('ens33',3))
+s.bind(('ens33', 3))
 
 while True:
     message = s.recv(1024)
-    print(repr(message)
+    print(repr(message))
     print(message.hex())
